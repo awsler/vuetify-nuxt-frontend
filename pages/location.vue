78da8106c4bd239d873dbf139d0197ff5ca1285c
ttmapr <martin.prussak@tracetronic.de>
2019-03-01 13:03:43 +0100

    Dummy image for location added.

--- pages/location.vue
+++ pages/location.vue
@@ -1,7 +1,8 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      Ein Raum-/Lageplan wird später hier angezeigt.
+      <span>Ein Raum-/Lageplan wird später hier angezeigt.</span>
+      <v-img src="nikolaus.svg" />
     </v-flex>
   </v-layout>
 </template>
