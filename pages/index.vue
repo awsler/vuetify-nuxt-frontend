c69333ac326aa42b2dce2d6fd0cd1d5ad34ae30c
ttmapr <martin.prussak@tracetronic.de>
2019-03-21 08:08:20 +0100

    Added times to the event list. Events with the same start time will be grouped, and the raw list of events will be sorted in advance.

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,41 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <v-card v-for="(event, i) in events" :key="i" elevation="5" style="margin-bottom: 1em">
-        <v-card-title style="font-weight:bold">{{ event.description }}</v-card-title>
-        <v-chip v-if="event.speaker!=null"><v-icon>person</v-icon>{{ event.speaker }}</v-chip>
-        <v-chip color="green">{{ event.location }}</v-chip>
-      </v-card>
+      <div v-for="(event, i) in events" :key="i">
+        <div v-if="!event.sameTime" class="time-info">
+          {{ event.startTimeString }} Uhr
+        </div>
+        <v-card elevation="5" style="margin-bottom: 1em">
+          <v-card-title style="font-weight:bold">
+            {{ event.description }}
+          </v-card-title>
+          <v-chip v-if="event.speaker!=null">
+            <v-icon>person</v-icon>
+            {{ event.speaker }}
+          </v-chip>
+          <v-chip color="green">
+            {{ event.location }}
+          </v-chip>
+        </v-card>
+      </div>
     </v-flex>
   </v-layout>
 </template>
+
+<style>
+.time-info {
+  text-align: left;
+  font-size: 150%;
+  padding: 0.25em 0.75em;
+}
+</style>
 
 <script>
 export default {
   name: 'Agenda',
   data() {
     return {
-      events: [
+      rawEvents: [
         { speaker: null, description: 'Frühstück', location: 'Buffet', startTime: new Date('2019-06-20T07:30:00+02:00'), endTime: new Date('2019-06-20T09:15:00+02:00') },
         { speaker: 'Anton Angermeier', description: 'Eröffnungsrede', location: 'Plenum', startTime: new Date('2019-06-20T09:30:00+02:00'), endTime: new Date('2019-06-20T09:45:00+02:00') },
         { speaker: 'Berta Baumann', description: 'Historischer Rückblick', location: 'Plenum', startTime: new Date('2019-06-20T09:45:00+02:00'), endTime: new Date('2019-06-20T10:45:00+02:00') },
@@ -28,6 +48,22 @@ export default {
         { speaker: null, description: 'Kaffeepause', location: 'Café', startTime: new Date('2019-06-20T15:15:00+02:00'), endTime: new Date('2019-06-20T16:00:00+02:00') },
         { speaker: null, description: 'Abendveranstaltung - Überraschung ;-)', location: 'wird noch bekanntgegeben', startTime: new Date('2019-06-20T17:00:00+02:00'), endTime: new Date('2019-06-20T20:00:00+02:00') }
       ]
+    }
+  },
+  computed: {
+    events() {
+      const result = []
+      let lastEvent = null
+      this.rawEvents.slice().sort((a, b) => {
+        return a.startTime - b.startTime
+      }).forEach((event) => {
+        const newEvent = { ...event }
+        newEvent.sameTime = (lastEvent && event.startTime.getTime() === lastEvent.startTime.getTime())
+        newEvent.startTimeString = event.startTime.getHours() + ':' + (event.startTime.getMinutes() < 10 ? '0' : '') + event.startTime.getMinutes()
+        result.push(newEvent)
+        lastEvent = event
+      })
+      return result
     }
   }
 }
